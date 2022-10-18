6a2a3697920f6ae5fcd441c4e3f0ca06c543cfa4
thbo <ewiben6000@gmail.com>
2022-10-18 05:39:11 +0100

    added the date and nav fxn for mobiles

--- js/script.js
+++ js/script.js
@@ -6,11 +6,32 @@ const h1 = document.querySelector(".heading-primary");
 console.log(myName);
 console.log(h1);
 
-h1.addEventListener("click", function () {
-  h1.textContent = myName;
-  h1.style.backgroundColor = "red";
-  h1.style.padding = "5rem";
-});
+// h1.addEventListener("click", function () {
+//   h1.textContent = myName;
+//   h1.style.backgroundColor = "red";
+//   h1.style.padding = "5rem";
+// });
+
+///////////////////////////////////////////////////////////
+// set current year
+const yearEl = document.querySelector('.year');
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
+// making the mobile nav work
+
+const btnNavEL = document.querySelector(".btn-mobile-nav");
+const headerEL = document.querySelector(".header");
+  
+btnNavEL.addEventListener('click', function(){
+  headerEL.classList.toggle('nav-open')
+})
+  
+  
+('nav-open');
+
+
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
